1e930019a84edd5c691fb166ea444f1b3339be42
NanthaKumara A/L Ganesan <nantha200576@gmail.com>
2026-09-22 00:31:35 +0800

    Add files via upload

---
+++ Task 4.2 python-docker-app/app.py
@@ -0,0 +1,13 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class SimpleHTTP(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(b"<html><body><h1>Hello from Python Docker Web Server! Task 4.2 Credit</h1></body></html>")
+
+if __name__ == "__main__":
+    server = HTTPServer(('0.0.0.0', 8080), SimpleHTTP)
+    print("Server running on port 8080...")
+    server.serve_forever()
